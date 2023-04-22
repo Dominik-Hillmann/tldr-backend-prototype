4ee9245906eb926a160371e08568226d0edd8248
Dominik-Hillmann <dominik.hillmann@gmx.de>
2023-04-22 23:01:42 +0200

    Parse PDFs

--- app/api/endpoints/summarize.py
+++ app/api/endpoints/summarize.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, File, UploadFile
+import pdfplumber
+from io import BytesIO
 
 router = APIRouter()
 
@@ -8,10 +10,17 @@ async def summarize(file: UploadFile = File(...)):
     """
     Endpoint that accepts a PDF file via a POST request.
     """
-    # contents = await file.read()
+    file_content = await file.read()
+    file_in_memory = BytesIO(file_content)
+
     # print(contents)
     print("Works")
+
+    with pdfplumber.open(file_in_memory) as pdf:
+        first_page = pdf.pages[0]
+
     # Here you can write code to process the PDF file
     return {
-        "filename": file.filename
+        "filename": file.filename,
+        "text": [ page.extract_text() for page in pdf.pages ]
     }
