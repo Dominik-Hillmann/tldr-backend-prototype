89727e18e4484b06fcf756f701acab215c3c0309
Dominik-Hillmann <dominik.hillmann@gmx.de>
2023-05-01 22:01:47 +0200

    Try to add questions

--- app/api/endpoints/summarize.py
+++ app/api/endpoints/summarize.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, File, UploadFile
+from fastapi import APIRouter, File, UploadFile, Form
 import pdfplumber
 from app.api.utils import get_token_len, break_up_tokens, detokenize
 from nltk import word_tokenize
@@ -9,69 +9,33 @@ from dotenv import load_dotenv
 import os
 import time
 from app.api.models import example_summary
+from app.api.utils import to_text_from_pdf
+from app.api.utils import summarize_by_chatgpt, query_doc_content
+from typing import List
+from langchain.memory import ChatMessageHistory
 
 router = APIRouter()
 load_dotenv()
 openai_key = os.getenv("OPENAI_KEY")
 
 @router.post("/summarize")
-async def summarize(file: UploadFile = File(...)):
+async def summarize(file: UploadFile = File(...), questions: List[str] = Form(...)):
     """
     Endpoint that accepts a PDF file via a POST request.
     """
 
+    print(questions)
+
     openai.api_key = openai_key
 
     file_in_memory = BytesIO(await file.read())
-    with pdfplumber.open(file_in_memory) as pdf:
-        pages = []
-        num_tokens_page = []
-        for i, page in enumerate(pdf.pages):
-            text_in_page = page.extract_text()
-            num_tokens = get_token_len(text_in_page)
-            num_tokens_page.append(num_tokens)
-            print(f"Number of tokens in page {i + 1}: {num_tokens}")
-            pages.append(text_in_page)
+    complete_text = " ".join(example_summary) # to_text_from_pdf(file_in_memory)
 
-    complete_text = ' '.join(pages)
-
-    prompt_response = []
-    encoding = tiktoken.get_encoding("gpt2")
-
-    chunks = break_up_tokens(word_tokenize(complete_text), 1000, 100)
-    # for i, chunk in enumerate(chunks):
-    #     # print(f"Number of tokens: {len(chunk)}.")
-    #     prompt_request = "Summarize this paper: " + detokenize(chunk)
-    #     messages = [{
-    #         "role": "system", 
-    #         "content": "This is text summarization."
-    #     }, {
-    #         "role": "user", 
-    #         "content": prompt_request
-    #     }]    
-
-    #     response = openai.ChatCompletion.create(
-    #         model="gpt-3.5-turbo",
-    #         messages=messages,
-    #         temperature=.5,
-    #         max_tokens=500,
-    #         top_p=1,
-    #         frequency_penalty=0,
-    #         presence_penalty=0
-    #     )
-        
-    #     print(response)
-    #     response_text = response["choices"][0]["message"]["content"]
-    #     prompt_response.append(response_text)
-    #     time.sleep(20)
-
-    # print(f"Number 'pure' tokens: {sum(num_tokens_page)}")
+    summary = summarize_by_chatgpt(complete_text)
+    q_a_pairs = [(question, query_doc_content(question)) for question in questions]
     
-    prompt_response = example_summary
-    full_response_text = " ".join(prompt_response)
-    print(prompt_response)
-
     return {
         "filename": file.filename,
-        "text": pages
+        "text": summary,
+        "q-a-pairs": q_a_pairs
     }
--- app/api/utils/__init__.py
+++ app/api/utils/__init__.py
@@ -1,4 +1,8 @@
+from io import BytesIO
 import nltk
+import pdfplumber
+import time
+import openai
 
 nltk.download('punkt')
 
@@ -26,3 +30,82 @@ def detokenize(tokenized_text: List[str]) -> str:
     detokenized_text = prompt_text.replace(" 's", "'s")
     return detokenized_text
 
+
+def to_text_from_pdf(file_in_memory: BytesIO) -> str:
+    with pdfplumber.open(file_in_memory) as pdf:
+        pages = []
+        num_tokens_page = []
+
+        for i, page in enumerate(pdf.pages):
+            text_in_page = page.extract_text()
+            num_tokens = get_token_len(text_in_page)
+            num_tokens_page.append(num_tokens)
+            print(f"Number of tokens in page {i + 1}: {num_tokens}")
+            pages.append(text_in_page)
+
+    complete_text = ' '.join(pages)
+    return complete_text
+
+
+def summarize_by_chatgpt(doc_text: str, max_num_tokens: int = 1000, overlap: int = 100) -> str:
+    chunks = break_up_tokens(word_tokenize(doc_text), max_num_tokens, overlap)
+    prompt_responses = []
+
+    for chunk in chunks:
+        # print(f"Number of tokens: {len(chunk)}.")
+        prompt_request = "Summarize this paper: " + detokenize(chunk)
+        messages = [{
+            "role": "system", 
+            "content": "This is text summarization."
+        }, {
+            "role": "user", 
+            "content": prompt_request
+        }]    
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+            temperature=.5,
+            max_tokens=500,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+        time.sleep(20)
+        
+        print(response)
+        response_text = response["choices"][0]["message"]["content"]
+        prompt_responses.append(response_text)
+
+    summary = " ".join(prompt_responses)
+
+    # Recursively summarize the text until we get a summary of fewer that a 1000 tokens.    
+    if get_token_len(summary) > 1000:
+        return summarize_by_chatgpt(summary)
+    else:
+        return summary
+    
+
+def query_doc_content(question: str) -> str:
+    messages = [{
+        "role": "system", 
+        "content": "Please answer this questions about the text you summarized earlier. Do not " +
+        "explain why you answer this way."
+    }, {
+        "role": "user", 
+        "content": question
+    }]    
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=.5,
+        max_tokens=500,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    time.sleep(20)
+
+    response_text = response["choices"][0]["message"]["content"]
+    return response_text    
